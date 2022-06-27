f2d6b0ae11c573ce6273d51cfaf507df22f01d3e
Megan Masanz <memasanz@microsoft.com>
2022-06-27 15:16:52 -0500

    updated external tables

---
+++ mm-synapse/External Resources/raw_mmxsynapsexdev_dfs_core_windows_net.sql
@@ -0,0 +1,6 @@
+﻿CREATE EXTERNAL DATA SOURCE [raw_mmxsynapsexdev_dfs_core_windows_net]
+    WITH (
+    TYPE = HADOOP,
+    LOCATION = N'abfss://raw@mmxsynapsexdev.dfs.core.windows.net'
+    );
+
---
+++ mm-synapse/dbo/External Tables/siteinfo.sql
@@ -0,0 +1,14 @@
+﻿CREATE EXTERNAL TABLE [dbo].[siteinfo] (
+    [Site] NVARCHAR (4000) NULL,
+    [SiteName] NVARCHAR (4000) NULL,
+    [SiteLocation] NVARCHAR (4000) NULL,
+    [State] NVARCHAR (4000) NULL
+)
+    WITH (
+    DATA_SOURCE = [raw_mmxsynapsexdev_dfs_core_windows_net],
+    LOCATION = N'SiteInfo.csv',
+    FILE_FORMAT = [SynapseDelimitedTextFormat],
+    REJECT_TYPE = VALUE,
+    REJECT_VALUE = 0
+    );
+
