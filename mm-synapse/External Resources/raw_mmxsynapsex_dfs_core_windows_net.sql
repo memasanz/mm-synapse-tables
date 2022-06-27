d93aebbc908956cd2d27799c1ca1d01efe50a01f
Megan Masanz <memasanz@microsoft.com>
2022-06-27 18:35:16 -0500

    updated

--- mm-synapse/External Resources/raw_mmxsynapsex_dfs_core_windows_net.sql
+++ mm-synapse/External Resources/raw_mmxsynapsex_dfs_core_windows_net.sql
@@ -1,6 +1,8 @@
-﻿CREATE EXTERNAL DATA SOURCE [raw_mmxsynapsex_dfs_core_windows_net]
-    WITH (
-    TYPE = HADOOP,
-    LOCATION = '$(externallocation)'
-    );
-
+﻿IF ('$(externallocation)' = 'qa')
+BEGIN
+    CREATE EXTERNAL DATA SOURCE [raw_mmxsynapsex_dfs_core_windows_net]
+        WITH (
+        TYPE = HADOOP,
+        LOCATION = N'abfss://raw@mmxsynapsexqa.dfs.core.windows.net'
+        );
+END
