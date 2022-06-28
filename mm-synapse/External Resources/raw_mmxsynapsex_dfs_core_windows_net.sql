6f9c2283374494cf75455b65cc42a18c5a344a3a
Megan Masanz <memasanz@microsoft.com>
2022-06-27 19:03:09 -0500

    updated

--- mm-synapse/External Resources/raw_mmxsynapsex_dfs_core_windows_net.sql
+++ mm-synapse/External Resources/raw_mmxsynapsex_dfs_core_windows_net.sql
@@ -3,7 +3,7 @@ BEGIN
     CREATE EXTERNAL DATA SOURCE [raw_mmxsynapsex_dfs_core_windows_net]
     WITH (
     TYPE = HADOOP,
-    LOCATION = N'abfss://raw@mmxsynapsexdev.dfs.core.windows.net'
+    LOCATION = N'abfss://raw@mmxsynapsexqa.dfs.core.windows.net'
     );
 
 
