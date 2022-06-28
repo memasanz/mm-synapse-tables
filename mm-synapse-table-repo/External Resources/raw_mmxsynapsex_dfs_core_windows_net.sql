5a93d8dca2690b74adbfba91bc0c9ad8314cc451
Megan Masanz <memasanz@microsoft.com>
2022-06-28 00:09:58 -0500

    uda

--- mm-synapse-table-repo/External Resources/raw_mmxsynapsex_dfs_core_windows_net.sql
+++ mm-synapse-table-repo/External Resources/raw_mmxsynapsex_dfs_core_windows_net.sql
@@ -1,6 +1,7 @@
-﻿CREATE EXTERNAL DATA SOURCE [raw_mmxsynapsex_dfs_core_windows_net]
+﻿if '$(envs)' = 'qa'
+CREATE EXTERNAL DATA SOURCE [raw_mmxsynapsex_dfs_core_windows_net]
     WITH (
     TYPE = HADOOP,
-    LOCATION = N'abfss://raw@mmxsynapsexdev.dfs.core.windows.net'
+    LOCATION = N'abfss://raw@mmxsynapsexqa.dfs.core.windows.net'
     );
 
